e551fb76c5e612589710c9da619ed32de421d3da
mrobbeloth-pdi <mrobbeloth@pdiarm.com>
2016-05-31 12:09:32 -0400

    Terminal Emulator expects jackpal-termexec2 shared library not termexec

--- libtermexec/Android.mk
+++ libtermexec/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := libtermexec
+LOCAL_MODULE := libjackpal-termexec2
 LOCAL_MODULE_TAGS := optional
 LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_SRC_FILES := src/main/jni/process.cpp
